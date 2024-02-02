4c30cf82b3482f6cfebc3517cc0b1c794b62fb82
Chelsey Rochester <cjankord43@yahoo.com>
2024-02-02 12:07:17 -0600

    add context file

---
+++ tv-tracker-series/src/store/series-data-context.jsx
@@ -0,0 +1,33 @@
+import { createContext, useState } from "react";
+
+const seriesCtx = createContext({
+    postItem: [],
+});
+
+// eslint-disable-next-line react/prop-types
+export function SeriesContextProvider({children}) {
+    // eslint-disable-next-line no-unused-vars
+    const [seriesData, setSereiesData] = useState();
+    const [modalIsVisible, setModalIsVisible] = useState(false);
+
+    function showModalHandler() {
+      setModalIsVisible(true);
+    }
+  
+    function hideModalHandler() {
+      setModalIsVisible(false);
+  }
+
+    const ctxValue = {
+        items: seriesData.title,
+        onCreatePost: showModalHandler,
+        isPosting: modalIsVisible,
+        onStopPosting: hideModalHandler
+    }
+
+    return (    
+        <seriesCtx.Provider value={ctxValue}>
+            {children}
+        </seriesCtx.Provider>
+    )
+}
