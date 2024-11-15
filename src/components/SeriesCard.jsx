4a095c5e6597433b712bb63edb68b452dd49ec77
Chelsey Rochester <cjankord43@yahoo.com>
2024-11-14 20:25:15 -0600

    change background and add data from api to card display

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -17,6 +17,16 @@ function SeriesCard({ series }) {
                     <h2 className="title">{series.name || 'Title not available'}</h2>
                     <p className="description">{series.summary ? series.summary.replace(/<\/?[^>]+(>|$)/g, "") : 'Description not available'}</p>
                     <p className="release">{series.premiered || 'Release date not available'}</p>
+                    <p className="site">{series.officialSite ? 
+                            <a href={series.officialSite} target="_blank" rel="noopener noreferrer">Visit Official Site</a> : 
+                            'Site not available'}</p>
+                    <p className="schedule">
+                            {series.status === 'Ended' ? 
+                            `Ended: ${series.ended}` : 
+                            (series.schedule && series.schedule.days.length > 0 ? 
+                            `Schedule: ${series.schedule.days.join(', ')}${series.schedule.time ? ` at ${series.schedule.time}` : ''}` : 
+                            'No schedule available')}
+                    </p>
                 </>
             )}    
         </div>
